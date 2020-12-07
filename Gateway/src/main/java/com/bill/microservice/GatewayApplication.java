e6e282b80228adca64a83654030ca74e618d953f
Bill_Chou <bill.chou@tpisoftware.com>
2020-12-07 09:48:36 +0800

    route設定改由程式控制

--- Gateway/src/main/java/com/bill/microservice/GatewayApplication.java
+++ Gateway/src/main/java/com/bill/microservice/GatewayApplication.java
@@ -3,7 +3,11 @@ package com.bill.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,6 +15,29 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		Builder routes = builder.routes();
+		
+		routes = createRoute(routes, "lb://MICROSERVICE1", "service1_route", "/service1/**");
+		
+		return routes.build();
+	}
+	
+	private Builder createRoute(Builder routes, String uri, String id, String path) {
+		return routes.route(id, r -> r.path(path)
+//				.filters(f -> f.modifyRequestBody(String.class, Object.class, MediaType.APPLICATION_JSON_VALUE,
+//						(exchange, s) -> {
+//							return null;
+//						})
+//				.modifyResponseBody(String.class, Object.class, MediaType.APPLICATION_JSON_VALUE,
+//						(exchange, s) -> {
+//							return null;
+//						})
+//				)
+				.uri(uri));
 	}
 
 }
